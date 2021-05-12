b302d51efb6f0b6e78a354be985b849ec855aaf9
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-05-11 18:14:44 -0600

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import movieData from '../sample-data'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
