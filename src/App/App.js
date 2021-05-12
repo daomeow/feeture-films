452c139c8b0cd5cad4d58de689b0463ea4346538
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-05-11 18:33:28 -0600

    Add src files

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
-import movieData from '../sample-data'
+import React, { Component } from 'react';
+import movieData from '../sample-data';
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: []
+    // view: list or movie
+    }
+  }
+  
   render() {
     return (
-      <div>
-        
-      </div>
-    )
-  }
+      <main className='App'>
+        <h1>Rancid Tomatillos</h1>touch 
+        <List movies={this.state.movies} />
+        </main>
+          )
+    }
 }
