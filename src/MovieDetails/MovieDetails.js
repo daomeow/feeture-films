9af38e1f54e6b32651edc1f26b28116b9bb846fd
Melanie Daoheuang <daoh0031@gmail.com>
2021-05-13 21:29:44 -0600

    Style details container

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -6,28 +6,25 @@ const MovieDetails =({ movieInfo }) => {
   console.log(movieInfo)
   const { poster_path, title, average_rating, release_date, backdrop_path } = movieInfo;  
   return (
-    <div className='movieDetails'>
+    <div className='movie-details'>
       <div className='details-container'>
-        <img src={poster_path} alt='Movie poster' class='details-poster'/>
-        <div className='general-info'>
-          <h3>{title}</h3>
-          <p class='tagline'>The flower that blooms..</p>
-          <p>{average_rating}</p>
-          <div class='details-data'>
-            <p>Release Date: {release_date}</p>
-            <p>120 minutes</p>
+        <div className='top-details'>
+          <img src={poster_path} alt='Movie poster' className='details-poster'/>
+          <div className='general-info'>
+            <h3>{title}</h3>
+            <p className='tagline'>The flower that blooms..</p>
+            <p>{average_rating}</p>
+            <div className='details-data'>
+              <sub>{release_date}</sub><sub>120 minutes</sub>
+            </div>
           </div>
         </div>
-        <p>OVERVIEW</p>
-        <div className='details-money'>
-          <p>Budget: $1M</p>
-          <p>Revenue: $1M</p>
-        </div>
-        <div className='details-genre'>
-          <p>Genres: Action</p>
-        </div>
+        <p>OVERVIEW OVERVIEW OVERVIEW OVERVIEW OVERVIEW</p>
+        <p>Budget: $1M</p>
+        <p>Revenue: $1M</p>
+        <p>Genres: Action</p>
       </div>
-      <img src={backdrop_path} alt='Movie backdrop' class='backdrop'/>
+      <img src={backdrop_path} alt='Movie backdrop' className='backdrop'/>
     </div>
   
   )
