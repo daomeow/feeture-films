80b84f78e6ab58034cff3ca75e296e927c2e3339
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-05-13 21:54:42 -0600

    Add styling ot list view

--- src/List/List.js
+++ src/List/List.js
@@ -3,24 +3,21 @@ import Movie from '../Movie/Movie';
 import './List.css';
 
 const List = ({movies, onClick}) => {
-// refactor to not use movie component and simply display posters
-// only use the movie component on click of a poster
-const movieCards = movies.map(movie => (
+  const movieCards = movies.map(movie => (
     <Movie
       posterPath={movie.poster_path}
       title={movie.title}
-      onClick={onClick}
+      onClick={onClick} // this should be adjusted
       id={movie.id}
       key={movie.id}
     />
-   ))
+  ))
 
-   return (
-     <div className='movies-container'>
-       {movieCards}
-     </div>
-   )
- 
- }
+  return (
+    <div className='movies-container'>
+      {movieCards}
+    </div>
+  )
+}
 
  export default List;
