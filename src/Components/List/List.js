967192e665a2717dc40e184e2157d6f7d73485a9
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-05-17 17:36:42 -0600

    Refactor List as class component

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import Movie from '../Movie/Movie';
-import './List.css';
+// import { react } from "@babel/types";
+// import { render } from "ejs";
+import React, { Component } from "react";
+import Movie from "../Movie/Movie";
+import "./List.css";
 
-const List = ({movies, onClick}) => {
-  const movieCards = movies.map(movie => (
-    <Movie
-      posterPath={movie.poster_path}
-      title={movie.title}
-      onClick={onClick} // this should be adjusted
-      id={movie.id}
-      key={movie.id}
-    />
-  ))
+export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <div className='movies-container'>
-      {movieCards}
-    </div>
-  )
+  render() {
+    const movieCards = this.props.movies.map((movie) => (
+      <Movie
+        posterPath={movie.poster_path}
+        title={movie.title}
+        onClick={this.props.onClick} // this should be adjusted
+        id={movie.id}
+        key={movie.id}
+      />
+    ));
+
+    return <div className="movies-container">{movieCards}</div>;
+  }
 }
 
- export default List;
+
