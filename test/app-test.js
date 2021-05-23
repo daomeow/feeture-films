1cf93c302923617feb6f494661eb4d45cd2b806b
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-05-22 20:51:32 -0600

    Initial commit

---
+++ test/app-test.js
@@ -0,0 +1,14 @@
+const chai = require("chai");
+const expect = chai.expect;
+const App = require("../src/components/App/App.js")
+
+const sampleData = require('../sample-data');
+
+describe('App', function() {
+  let app, list, movie;
+
+  beforeEach(function() {
+    app = new App()
+  })
+
+})
